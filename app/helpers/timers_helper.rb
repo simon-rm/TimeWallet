256c6340534e268e786a283177d939ad9f2cea57
simon <simonrmurcia@gmail.com>
2025-05-26 17:09:22 +0000

    format duration in 3.5h format in days view

--- app/helpers/timers_helper.rb
+++ app/helpers/timers_helper.rb
@@ -8,4 +8,11 @@ module TimersHelper
       :old
     end
   end
+
+  def format_duration(seconds)
+    hours = seconds / 3600.0
+    rounded_hours = (hours * 2).round / 2.0 # rounds to .5 or .0
+    rounded_hours = rounded_hours.to_i if rounded_hours.to_s.ends_with?(".0")
+    "#{rounded_hours}h"
+  end
 end
